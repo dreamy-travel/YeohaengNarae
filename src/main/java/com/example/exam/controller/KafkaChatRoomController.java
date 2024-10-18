7a35860448ebd11d67f52f0c7a430dab813e5afc
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2024-10-18 19:54:38 +0900

    kafka 수정 2
    Controller -> RestController

--- src/main/java/com/example/exam/controller/KafkaChatRoomController.java
+++ src/main/java/com/example/exam/controller/KafkaChatRoomController.java
@@ -3,41 +3,39 @@ package com.example.exam.controller;
 import com.example.exam.model.KafkaChatRoom;
 import com.example.exam.repository.KafkaChatRoomRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/kafkachat")
+@RestController
 public class KafkaChatRoomController {
     private final KafkaChatRoomRepository chatRoomRepository;
 
-    @GetMapping("/room")
+    @GetMapping("/kafkachat/room")
     public String rooms(Model model) {
         return "/kafkachat/room";
     }
 
-    @GetMapping("/rooms")
+    @GetMapping("/kafkachat/rooms")
     @ResponseBody
     public List<KafkaChatRoom> room() {
         return chatRoomRepository.findAllRoom();
     }
 
-    @PostMapping("/room")
+    @PostMapping("/kafkachat/room")
     @ResponseBody
     public KafkaChatRoom createRoom(@RequestParam("name") String name) {
         return chatRoomRepository.createChatRoom(name);
     }
 
-    @GetMapping("/room/enter/{roomId}")
+    @GetMapping("/kafkachat/room/enter/{roomId}")
     public String roomDetail(Model model, @PathVariable("roomId") String roomId) {
         model.addAttribute("roomId", roomId);
         return "/kafkachat/roomdetail";
     }
 
-    @GetMapping("/room/{roomId}")
+    @GetMapping("/kafkachat/room/{roomId}")
     @ResponseBody
     public KafkaChatRoom roomInfo(@PathVariable("roomId") String roomId) {
         return chatRoomRepository.findRoomById(roomId);
